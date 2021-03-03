ff97a354ebd8b0d49579ad6edd2d55bd8d7c38fb
Sakharuk Viktar <natgeogr93@gmail.com>
2021-03-03 17:22:53 +0300

    feat: deploy on gh-pages

--- game-2048/src/components/Footer.js
+++ game-2048/src/components/Footer.js
@@ -5,10 +5,25 @@ export default function Footer() {
 
   return (
     <div className='footer'>
-      Created by<a className='footer-link' href='https://github.com/natgeo89' rel='noreferrer' target='_blank'>Viktar Sakharuk</a>
+      Created by
+      <a
+        className='footer-link'
+        href='https://github.com/natgeo89'
+        rel='noreferrer'
+        target='_blank'>
+          Viktar Sakharuk
+      </a>
       for
-      <a className='footer-link' href='https://rs.school/js/' rel='noreferrer' target='_blank'><img className='footer-image' src={rss}
-        alt='Rolling Scope School'/></a>
+      <a 
+        className='footer-link'
+        href='https://rs.school/js/'
+        rel='noreferrer'
+        target='_blank'>
+          <img
+            className='footer-image'
+            src={rss}
+            alt='Rolling Scope School'/>
+      </a>
       <span>2021</span>
     </div>
   )
